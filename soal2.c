69480b249ffc073fb24f7eb291303dcd4487a957
Zahra Aulia <13224007@mahasiswa.itb.ac.id>
2026-05-04 10:43:45 +0700

    Update soal2.c

--- soal2.c
+++ soal2.c
@@ -13,19 +13,20 @@ struct Node {
     struct Node* next;
 };
 
-struct Node* new_Node(int data) {
+struct Node* newNode(int data) {
     struct Node* node = (struct Node*) malloc(sizeof(struct Node));
     node->data = data;
     node->next = NULL;
     return node;
 }
 
-struct Node* mergeTwoLists(struct Node* head1, struct Node* head2) {
+struct Node* mergelists(struct Node* head1, struct Node* head2) {
     if (!head1) return head2;
     if (!head2) return head1;
 
     struct Node temp;
     struct Node* tail = &temp;
+    temp.next = NULL;
 
     while (head1 && head2) {
         if (head1->data < head2->data) {
@@ -38,36 +39,45 @@ struct Node* mergeTwoLists(struct Node* head1, struct Node* head2) {
         tail = tail->next;
     }
 
-    tail->next = head1 ? head1 : head2;
+    if (head1) tail->next = head1;
+    if (head2) tail->next = head2;
+
     return temp.next;
 }
 
 void display(struct Node* head) {
-    if (head == NULL) { 
+    if (head == NULL) {
         printf("MERGED EMPTY");
     } else {
-    struct Node *temp = head;
-    while (head) {
-        printf("%d ", head->data);
-        head = head->next;
+        struct Node *temp = head;
+        while (temp) {
+            printf("%d ", temp->data);
+            temp = temp->next;
+        }
     }
     printf("\n");
+}
+
+struct Node* createlist(int n) {
+    if (n <= 0) return NULL;
+    int x;
+    scanf("%d", &x);
+    struct Node* head = newNode(x);
+    struct Node* temp = head;
+    for (int i = 1; i < n; i++) {
+        scanf("%d", &x);
+        temp->next = newNode(x);
+        temp = temp->next;
     }
+    return head;
 }
 
 int main() {
-    struct Node *list1, *list2;
     int n, m;
-    scanf("%d %d %d %d",n, &list1, m, &list2);
-
-	struct Node* result = mergeTwoLists(list1, list2);
+    if (scanf("%d %d", &n, &m) != 2) return 0;
+    struct Node* list1 = createlist(n);
+    struct Node* list2 = createlist(m);
+    struct Node* result = mergelists(list1, list2);
     display(result);
-
     return 0;
 }
-
-/* REFERENSI
-1. https://www.w3resource.com/c-programming-exercises/linked_list/c-linked_list-exercise-35.php
-2. https://stackoverflow.com/questions/2348374/merging-two-sorted-linked-lists
-3. https://www.youtube.com/watch?v=zzk9BQro7bA
-*/
